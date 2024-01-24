f30e59e6f8acc5819eca2b211669312afcd6f81d
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-01-24 16:56:29 +0200

    Refactor

--- test/test_Birthday.py
+++ test/test_Birthday.py
@@ -8,6 +8,7 @@ import pytest
 bd = Birthday(date=datetime.strptime("21-11-1978", "%d-%m-%Y"))
 
 assert bd.local_str == "21-11-1978"
+
 assert bd.date == datetime.strptime("21-11-1978", "%d-%m-%Y").date()
 
 with pytest.raises(ValidationError, match="Date should be in the past"):
