843051a046286ec72a16351dd3a5e0b62c51c9e6
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-02-02 16:14:24 +0200

    Update

--- pimp/cls/PimpConfig.py
+++ pimp/cls/PimpConfig.py
@@ -1,3 +1,47 @@
-from abcs.AbcBook import Singleton
+from typing import Dict
+from pathlib import Path
+from cls.AddressBook import AddressBook
+from cls.NoteBook import Notebook
+from abcs.DataProviderABC import DataProvider
+from data_providers import PickleDataProvider, JsonDataProvider
+import yaml
 
-class PimpConfig(Singleton):
+
+class Configleton(type):
+    _instances = {}
+
+    def __call__(self, *args, **kwargs):
+        if self not in self._instances:
+            self._instances[self] = super().__call__(*args, **kwargs)
+        return self._instances[self]
+
+
+class PimpConfig(metaclass=Configleton):
+    """Main config storage for all app`s needs"""
+    address_book: AddressBook = None
+    note_book: Notebook = None
+
+    __address_book_dp: DataProvider = None
+    __note_book_dp: DataProvider = None
+
+    __data_providers: Dict[str, DataProvider] = \
+        {"file:pickle": PickleDataProvider.PickleDataProvider,
+         "file:json": JsonDataProvider.JsonDataProvider}
+
+    def __init__(self, config_path) -> None:
+        super().__init__()
+        self.__read_config(config_path)
+
+    @staticmethod
+    def __read_config(path):
+        if Path(path).exists():
+            with Path(path).open("r", encoding="utf-8") as fin:
+                config = yaml.safe_load(fin)
+        print(config)
+
+    def refresh_data(self):
+        pass
+
+
+if __name__ == "__main__":
+    pimp_config = PimpConfig(config_path="../config.yaml")
